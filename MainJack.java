6da16164259ef123f1c37fd07ee0c8718cf7d173
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-05-29 14:51:39 -0400

    Update MainJack.java

--- MainJack.java
+++ MainJack.java
@@ -1,22 +1,52 @@
+
 import java.util.*;
-// where i call classes made in the the BlackJack file
+
+
 public class MainJAck {
  
+  public static void HitStand (BlackJack myCard) {
+    Scanner scanner = new Scanner(System.in);
+    //call the hit function to add another card for the player
+    switch (scanner.nextInt()){
+     case 1:
+    	 myCard.Hit();
+   	     HitStand(myCard);
+   	     
+     case 2:
+    	 //myCard.Dealer();
+    	 myCard.Stand();
+   	     //myCard.Dealer();
+   	     return;
+    }
+    HitStand(myCard);
+    
+    /**
+	if (scanner.next().equals("hit")) {
+	  myCard.Hit();
+	  HitStand(myCard);
+	}else if (scanner.next().equals("stand")) {
+	  System.out.println("not");
+	  myCard.Stand();
+	  myCard.Dealer();
+	}else {
+		System.out.println("Invalid Input");
+	}
+	HitStand(myCard); */
+  }
  public static void main(String[] args) {
-  
-  Scanner scanner = new Scanner(System.in);
+	 System.out.println("Type in '1' for hit and '2' to stand");
+  //Scanner scanner = new Scanner(System.in);
   BlackJack card = new BlackJack ();
   
   
+  //push the letter K,Q,J and A into the arrayList
   card.push("J");
   card.push("Q");
   card.push("K");
   card.push("A");
 
-  
-  
-  
-  //adding number 2 to 9, four times in the arraylist
+
+  //adding number 2 to 9, four times in the arrayList
   for (int i = 2; i < 10; i++) {
    card.push(Integer.toString(i));
   }
@@ -24,31 +54,10 @@ public class MainJAck {
   card.shuffle();
   card.Player();
   card.Dealer();
-  
-  while (scanner.next().equals("hit") || scanner.next().equals("stand")) {
-	  if (scanner.next().equals("hit")) {
-		  card.Hit();
-		  card.Dealer();  
-	  }else if (scanner.next().equals("stand")) {
-		  System.out.print("Stop");
-		  card.Stand();
-		  card.Dealer();
-	  }else {
-		  break; 
-	  }
 	  
-	  
-  }
   
-  
-  /**
-  while (scanner.next().equals("stand")) {
-	  System.out.print("Stop");
-	  card.Stand();
-	  card.Dealer();
-  }*/
-  
-  
+  HitStand(card);
  }
+ 
  
 }
